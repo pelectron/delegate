16ba84c20cd6ec86a6ce613537a3e622f3dd07b1
pelectron <43130018+pelectron@users.noreply.github.com>
2022-03-18 23:38:53 +0100

    fixed build error

--- include/multicast_delegate.hpp
+++ include/multicast_delegate.hpp
@@ -9,7 +9,6 @@
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * https://www.boost.org/LICENSE_1_0.txt) 
- * \tableofcontents
  */
 #ifndef PC_MULTICAST_DELEGATE_HPP
 #define PC_MULTICAST_DELEGATE_HPP
